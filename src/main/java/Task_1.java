30a7475cd0e3dfed5505fb76c36ba1cd229d7823
Serhii <sergeygoryushko17@gmail.com>
2019-10-19 23:49:49 +0300

    I added to the snippet of code the custom exception.

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -2,26 +2,26 @@
 // if number > 0 print “Number > 0”
 // if number = 0 throw your own exception
 
-
 public class Task_1 {
+
     static int number = 0;
 
-    public static void main(String[] args) {
-
-
-        try {
-            if (number < 0) {
-                System.out.println("Number < 0");
-            }
-            if (number > 0) {
-                System.out.println("Number > 0");
-            }
-            if (number == 0) {
-                throw new Exception("Zero is a deprecated value. Please, enter another one.");
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws MyException {
+        if (number < 0) {
+            System.out.println("Number < 0");
+        }
+        if (number > 0) {
+            System.out.println("Number > 0");
+        }
+        if (number == 0) {
+            throw new MyException("This number prohibits to be entered: " + number);
+        }
+    }
+    static class MyException extends Exception {
+        public MyException(String message) {
+            super(message);
         }
     }
 }
+
+
